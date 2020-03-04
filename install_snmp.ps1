66f2be552a62d4b9ce8d3a098490f1bcc0ba4517
Benjamin Ritter <britter@LM-BRITTER.local>
2020-03-04 09:52:32 -0600

    made change to snmp string

---
+++ install_snmp.ps1
@@ -0,0 +1,23 @@
+
+
+Install-windowsfeature -name SNMP-SERVICE
+
+Start-Sleep -Seconds 30
+
+Install-WindowsFeature RSAT-SNMP
+
+Start-Sleep -Seconds 30
+
+$registryPath = "HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\ValidCommunities"
+Set-Location -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\ValidCommunities'
+
+
+New-ItemProperty -Path $registryPath -Name netman -Value 4 `
+
+    -PropertyType DWORD -Force | Out-Null 
+
+end
+
+
+
+Remove-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\PermittedManagers -Name 1
